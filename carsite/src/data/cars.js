1eab8b839189f82b3271d0acd8ff87ce3005d02d
Zarylbekov Adis <zarylbekov_a@IUCA.LOC>
2025-12-15 16:27:05 +0600

    ad

--- carsite/src/data/cars.js
+++ carsite/src/data/cars.js
@@ -18,14 +18,14 @@ export const cars = [
     model: "Taycan",
     name: "Porsche Taycan 4S",
     year: 2022,
-    img: "https://i.pinimg.com/736x/ed/d9/26/edd9265e6ccd10fab314b0f07de9fa7d.jpg"
+    img: "https://a.storyblok.com/f/338913/1280x1024/e178b7a186/taycan-desktop_5-4.jpg/m/1800x0/filters:format(webp):quality(80)"
   },
   {
     id: 4,
     model: "Panamera",
     name: "Porsche Panamera GTS",
     year: 2021,
-    img: "https://i.pinimg.com/736x/82/ef/24/82ef24070e1f3485dc6a860e75ca6202.jpg"
+    img: "https://a.storyblok.com/f/338913/1280x1024/c06fa1b962/panamera-desktop_5-4.jpg/m/1800x0/filters:format(webp):quality(80)"
   }
 
   ];
